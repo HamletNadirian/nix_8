776c7a666f0a78de7634fa7028c502e1857e34fb
Hamlet Nadirian <gamlet.osmanov@gmail.com>
2022-01-07 17:27:53 +0200

    Запускать deploy.bat с помощью Run cmd script=)

---
+++ hw_7_web_jdbc/src/main/java/ua/com/alevel/Hw7WebJdbcApplication.java
@@ -0,0 +1,29 @@
+package ua.com.alevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.event.EventListener;
+import ua.com.alevel.config.jpa.DatasourceProperties;
+import ua.com.alevel.config.jpa.JpaConfig;
+
+
+@SpringBootApplication
+@EnableConfigurationProperties(DatasourceProperties.class)
+public class Hw7WebJdbcApplication {
+    private final JpaConfig jpaConfig;
+
+    public Hw7WebJdbcApplication(JpaConfig jpaConfig) {
+        this.jpaConfig = jpaConfig;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw7WebJdbcApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initDB() {
+        jpaConfig.connect();
+    }
+}
---
+++ hw_7_web_jdbc/src/test/java/ua/com/alevel/hw_7_web_jdbc/Hw7WebJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.alevel.hw_7_web_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw7WebJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
